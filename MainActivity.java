6f1b6ddd28ae7b8aa2cbc2bd1cf1fc00ce3e1d20
AaditiKale <161145323+AaditiKale@users.noreply.github.com>
2025-08-07 14:12:16 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.momsdiary;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
